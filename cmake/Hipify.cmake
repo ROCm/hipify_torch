b61141eab1f554546e3181f51cc02035e71ff065
Pruthvi Madugundu <pruthvigithub@gmail.com>
2021-08-24 11:15:43 -0700

    Adding CMAKE api to trigger hipify

--- cmake/Hipify.cmake
+++ cmake/Hipify.cmake
@@ -8,6 +8,10 @@ set(_temp_file_cuda_to_hip_list "cuda_to_hip_list")
 # Get the hipify_cli.py source directory from current directory by going to parent directory
 get_filename_component(HIPIFY_DIR ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
+include(CMakeParseArguments)
+
+# This is an internal function, NOT an API
+# TODO: Move this function into a separate file
 function(write_file_list FILE_SUFFIX INPUT_LIST)
   message(STATUS "Writing ${FILE_SUFFIX} into file - file_${FILE_SUFFIX}.txt")
   set(_FULL_FILE_NAME "${CMAKE_BINARY_DIR}/${_temp_file_cuda_to_hip_list}_${FILE_SUFFIX}.txt")
@@ -18,12 +22,16 @@ function(write_file_list FILE_SUFFIX INPUT_LIST)
   endforeach()
 endfunction()
 
+# This is an internal function, NOT an API
+# TODO: Move this function into a separate file
 function(get_file_list FILE_SUFFIX OUTPUT_LIST)
   set(_FULL_FILE_NAME "${CMAKE_BINARY_DIR}/${_temp_file_cuda_to_hip_list}_${FILE_SUFFIX}.txt")
   file(STRINGS ${_FULL_FILE_NAME} _FILE_LIST)
   set(${OUTPUT_LIST}_HIP ${_FILE_LIST} PARENT_SCOPE)
 endfunction()
 
+# This is an internal function, NOT an API
+# TODO: Move this function into a separate file
 function(update_list_with_hip_files FILE_SUFFIX)
   set(_SCRIPTS_DIR ${HIPIFY_DIR}/tools)
   set(_FULL_FILE_NAME "${CMAKE_BINARY_DIR}/${_temp_file_cuda_to_hip_list}_${FILE_SUFFIX}.txt")
@@ -39,6 +47,9 @@ function(update_list_with_hip_files FILE_SUFFIX)
   endif()
 endfunction()
 
+# Function -> get_hipified_list()
+# - INPUT_LIST - Input list containing cuda files
+# - OUTPUT_LIST - Returns an output list containing the HIP files
 function(get_hipified_list INPUT_LIST OUTPUT_LIST)
   string(RANDOM LENGTH 16 RAND_STRING)
   set(TEMP_FILE_NAME "tmp_${RAND_STRING}")
@@ -51,18 +62,45 @@ function(get_hipified_list INPUT_LIST OUTPUT_LIST)
   set(${OUTPUT_LIST} ${__temp_srcs_HIP} PARENT_SCOPE)
 endfunction()
 
-set(HIPIFY_COMMAND
-  ${HIPIFY_DIR}/hipify_cli.py
-  --project-directory ${PROJECT_SOURCE_DIR}
-  --output-directory ${PROJECT_SOURCE_DIR}
-  --dump-dict-file ${HIPIFY_DICT_FILE}
-)
+# Function -> hipify()
+# - Takes 3 optional arguments, either CUDA_SOURCE_DIR or CONFIG_FILE argument is required
+# - CUDA_SOURCE_DIR --> Full path of input cuda source directory which needs to be hipified.
+# - HIP_SOURCE_DIR --> Full path of output directory where the hipified files will be placed.
+#                      If not provided, it is set to CUDA_SOURCE_DIR.
+# - CONFIG_FILE --> JSON format file, which provides additional arguments for hipify_cli.py file.
+#                   When set, it is having higher precendence over CUDA_SOURCE_DIR/HIP_SOURCE_DIR.
+function(hipify)
+  set(flags)
+  set(singleValueArgs CUDA_SOURCE_DIR HIP_SOURCE_DIR CONFIG_FILE)
+  set(multiValueArgs)
 
-execute_process(
-  COMMAND ${HIPIFY_COMMAND}
-  RESULT_VARIABLE hipify_return_value
-)
-if (NOT hipify_return_value EQUAL 0)
-  message(FATAL_ERROR "Failed to hipify files!")
-endif()
+  cmake_parse_arguments(HIPIFY "${flags}" "${singleValueArgs}" "${mulitValueArgs}" ${ARGN})
 
+  if(HIPIFY_CONFIG_FILE)
+    set(HIPIFY_COMMAND
+      ${HIPIFY_DIR}/hipify_cli.py
+      --config-file ${HIPIFY_CONFIG_FILE}
+      --dump-dict-file ${HIPIFY_DICT_FILE}
+    )
+  elseif(HIPIFY_CUDA_SOURCE_DIR)
+    if(NOT HIPIFY_HIP_SOURCE_DIR)
+      set(HIPIFY_HIP_SOURCE_DIR ${HIPIFY_CUDA_SOURCE_DIR})
+    endif()
+    set(HIPIFY_COMMAND
+      ${HIPIFY_DIR}/hipify_cli.py
+      --project-directory ${HIPIFY_CUDA_SOURCE_DIR}
+      --output-directory ${HIPIFY_HIP_SOURCE_DIR}
+      --dump-dict-file ${HIPIFY_DICT_FILE}
+    )
+  else()
+    message(FATAL_ERROR "Wrong invocation, either CUDA_SOURCE_DIR or CONFIG_FILE input parameter is required")
+  endif()
+
+  execute_process(
+   COMMAND ${HIPIFY_COMMAND}
+   RESULT_VARIABLE hipify_return_value
+  )
+  if (NOT hipify_return_value EQUAL 0)
+    message(FATAL_ERROR "Failed to hipify files!")
+  endif()
+endfunction()
