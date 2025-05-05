e4372e791a72c1b5c4c2b4a0f21a605fb5a4910d
Jeff Daily <jeff.daily@amd.com>
2025-05-05 21:54:08 +0000

    update __all__ for v2, remove unused function

--- hipify_torch/v2/hipify_python.py
+++ hipify_torch/v2/hipify_python.py
@@ -72,7 +72,7 @@ custom_mapping_file = "custom_hipify_mappings.json"
 __all__ = ['InputError', 'openf', 'bcolors', 'GeneratedFileCleaner', 'match_extensions', 'matched_files_iter',
            'preprocess_file_and_save_result', 'compute_stats', 'add_dim3', 'processKernelLaunches', 'find_closure_group',
            'find_bracket_group', 'find_parentheses_group', 'replace_math_functions', 'hip_header_magic', 'replace_extern_shared',
-           'get_hip_file_path', 'is_out_of_place', 'is_pytorch_file', 'is_cusparse_file', 'is_special_file', 'is_caffe2_gpu_file',
+           'get_hip_file_path', 'is_out_of_place',
            'Trie', 'preprocessor', 'file_specific_replacement', 'file_add_header',
            'fix_static_global_kernels', 'extract_arguments', 'str2bool', 'CurrentState', 'HipifyResult', 'hipify']
 
@@ -607,21 +607,6 @@ def is_out_of_place(rel_filepath):
         return False
     return True
 
-
-# deprecated
-def is_pytorch_file(rel_filepath):
-    assert not os.path.isabs(rel_filepath)
-    if rel_filepath.startswith("aten/"):
-        if rel_filepath.startswith("aten/src/ATen/core/"):
-            return False
-        return True
-    if rel_filepath.startswith("torch/"):
-        return True
-    if rel_filepath.startswith("third_party/nvfuser/"):
-        return True
-    if rel_filepath.startswith("tools/autograd/templates/"):
-        return True
-    return False
 
 class TrieNode:
     """A Trie node whose children are represented as a directory of char: TrieNode.
